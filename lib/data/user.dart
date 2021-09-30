a82aa5e3e64df55173b828f1c1db0439269e0031
Josia Almeida <josiaalmeida0001@gmail.com>
2021-09-30 08:15:10 +0100

    Estruturando arquivos

---
+++ lib/data/user.dart
@@ -0,0 +1,22 @@
+import '../models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: "Josia Josué",
+      email: "josiaalmeida001@gmail.com",
+      avatarUrl:
+          'https://www.google.com/url?sa=i&url=https%3A%2F%2Ficon-library.com%2Ficon%2Favatar-icon-images-4.html&psig=AOvVaw0c_8i8pVdSoiBTpUncVYTh&ust=1633038010129000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOjhpvOSpfMCFQAAAAAdAAAAABAH'),
+  '2': const User(
+      id: '2',
+      name: "Josia Nawanga",
+      email: "josiaalmeida001@gmail.com",
+      avatarUrl:
+          'https://www.google.com/url?sa=i&url=https%3A%2F%2Ficon-library.com%2Ficon%2Favatar-icon-images-4.html&psig=AOvVaw0c_8i8pVdSoiBTpUncVYTh&ust=1633038010129000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOjhpvOSpfMCFQAAAAAdAAAAABAH'),
+  '3': const User(
+      id: '3',
+      name: "Josia Almeida",
+      email: "josiaalmeida001@gmail.com",
+      avatarUrl:
+          'https://www.google.com/url?sa=i&url=https%3A%2F%2Ficon-library.com%2Ficon%2Favatar-icon-images-4.html&psig=AOvVaw0c_8i8pVdSoiBTpUncVYTh&ust=1633038010129000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOjhpvOSpfMCFQAAAAAdAAAAABAH'),
+};
